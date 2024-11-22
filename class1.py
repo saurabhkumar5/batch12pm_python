170af54424481be36de15abfe4fb830badce4194
saurabh kumar <saurabhkumar9493@gmail.com>
2024-11-22 12:48:28 +0530

    first commit

---
+++ class1.py
@@ -0,0 +1,16 @@
+# print(12) 
+# print(78)
+# print(60)
+# print(98)
+
+# print(10+10)
+# print(12+12)
+# print(10*10)
+# print(100%9)
+
+
+# print('welcome')
+# print("hello world")
+
+
+
---
+++ class2_variables.py
@@ -0,0 +1,23 @@
+# speed = 30
+# speed = 40
+# print(speed)
+
+# score = 60
+# score = 80
+# scoree = 90
+# print(score)
+# print(scoree)
+
+# abc = 10
+# 2 == 100
+# _a = 10
+# print(_a)
+
+
+# age = 10
+# print(age)
+
+
+# a,b,c = 10,20,30
+# print(a,b,c)
+
