40559604275a578e2555459186c1b2c9fa87773f
saurabh kumar <saurabhkumar9493@gmail.com>
2024-11-23 13:01:28 +0530

    condition

---
+++ class3_dataTypes.py
@@ -0,0 +1,45 @@
+# data tyeps
+# 1. integers
+# 2. float
+# 3. strings
+# 4. booleans
+# 5. Lists
+# 6. tuples
+# 8. sets
+
+
+# a = 10 
+# a = 10.5 #float
+# a = "kumar"
+# a = False
+# print(type(a))
+
+
+
+# a = 10
+# a = 20
+# b = 30
+# a = b
+# print(a)
+
+
+# 10 >5  True
+# 8<3    False
+#  6>=6  True
+
+# print(5>=5)
+
+# print(17.3>17.03)
+# print(13.999<=13.199)
+
+# logical operator
+# 1.Logical and  operator
+# 2.Logical or  operator
+# 3.Logical not  operator
+
+
+# print(10>5 and 13>8 and 20>10 and 45>7)
+
+# print(10>6 or 32>33 or 65>78)
+
+# print(not 12>100)
---
+++ class4_condition.py
@@ -0,0 +1,34 @@
+# 1. if
+# 2. if-else
+# 3. elif
+# 4. nested if-else
+
+
+# age = 10
+
+# if age>18:
+#     print("you can vote")
+
+
+
+# age = 10
+# if age>8 or age<=6:
+#     print("you are child")
+
+
+# age = 10
+# if age>=18:
+#     print("you can vote")
+# else:
+#     print("u are underage")
+
+
+
+# a = 56.00
+# if a<=56.0 and 41>36:
+#     print("hello")
+# else:
+#     print("world")    
+
+# a  = input("enter any number")
+# print(a)
