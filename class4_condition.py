1b3da6cfd3e43cfd6e5d1d44959ae4da5d6cbb6b
saurabh kumar <saurabhkumar9493@gmail.com>
2024-11-25 12:55:06 +0530

    condition

--- class4_condition.py
+++ class4_condition.py
@@ -32,3 +32,81 @@
 
 # a  = input("enter any number")
 # print(a)
+
+
+# a = 10
+# if a>15:
+#     print("hello")
+# if a>8:
+#     print("world")    
+# if a>9:
+#     print("kullu")
+# else:
+#     print("byeee")        
+
+
+# a = 10
+# if a>15:
+#     print("hello")
+# elif a>18:
+#     print("world")    
+# elif a>19:
+#     print("kullu")
+# else:
+#     print("byeee") 
+
+
+
+# if 10>5:
+#     if 5>8:
+#         print("kullu")
+#     else:
+#         print("manali")  
+# else:
+#     print("byee")    
+
+
+
+
+# if 45>=450 and 23<=23.01:
+#     if 36>36:
+#         print("shimla")
+#     else:
+#         print("kullu")
+# else:
+#     if 12>10:
+#         print("delhi")       
+#     else:
+#         print("byeee")         
+
+
+
+# number = int(input("enter any number"))
+# if number>0:
+#     print("your number is +ve")
+#     if number%2==0:
+#         print("your number is even")
+#     else:
+#         print("your number is odd")    
+# else:
+#     print("your number is -ve")    
+#     if number%2==0:
+#         print("your number is even")
+#     else:
+#         print("your number is odd")    
+
+
+
+
+# print(2)
+# print(4)
+# print(6)
+
+# print(1)
+# print(2)
+# print(3)
+# print(4)
+# print(5)
+
+
+# ab start hota h loops
