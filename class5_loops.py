82959fa30ad4347676e85cfced668d0b26257eee
saurabh kumar <saurabhkumar9493@gmail.com>
2024-11-26 13:04:07 +0530

    for loop

---
+++ class5_loops.py
@@ -0,0 +1,61 @@
+# 1. for loop
+
+
+# for a in range(6):
+#     print(a)
+
+# for i in range(1,101):
+#     print(i)
+
+#     3 to 20 
+
+# for i in range(3,21):
+#     if i%2==0:
+#         print(i)
+
+# for i in range(0,21):
+#     if i%2==0:
+#         print(i)
+
+
+# 0*2 = 0
+# 1*2 = 2
+# 2*2 = 4
+# 3
+# 4
+# 5
+# 6
+# 7
+# 8
+# 9
+# 10
+
+# for i in range(0,11):
+#     print(i*3)
+
+# for i in range(0,31,3):
+#     print(i)
+
+# number = int(input("enter any number for table"))
+
+# for i in range(0,11):
+#     print(i*number)
+
+
+
+# for i in range(0,11):
+#     print(f"{i}*2 =")
+
+
+# 3 7 11 15 19
+
+# for i in range(3,20,4):
+#     print(i)
+
+
+# range(0,20)
+# 0 to 13
+
+for i in range(0,20):
+    # print(i) 0 to 13
+    # print(i)   12 17
